cb93b576603e8abfd27f7e63de9353b7960b6eaa
Zhanarys <37359098+Zhanarys27@users.noreply.github.com>
2023-06-24 12:57:57 +0600

    add folder ex_1, ex_2, gitignore

---
+++ ex_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+void Natural(int num)
+{
+    while(num>0)
+    {
+        if(num!=0)
+        {
+            System.Console.Write($"{num}, ");
+        }
+        num--;
+    }
+}
+
+
+string Natural2(int num)
+{
+    if(num == 1) return $"{num}";
+    else return $"{num}, " + Natural2(num-1) ;
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите значение: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Natural2(num));
+
---
+++ ex_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Summary(int num1, int num2)
+{
+    if(num1 == num2) return num2;
+    else return num2 + Summary(num1, num2 -1);
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите первичное значение");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите вторичное значение");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Summary(num1,num2));
